716475446296be91bc75ca934a2fdaf976c61e87
HuangFuGui <1151650717@qq.com>
2016-04-28 20:52:14 +0800

    hibernate one table map CRUD

--- Hibernate/hibernatedemo1/src/com/hibernate/user/UserTest.java
+++ Hibernate/hibernatedemo1/src/com/hibernate/user/UserTest.java
@@ -20,9 +20,9 @@ public class UserTest {
 		Configuration config = new Configuration().configure();
 		//创建服务注册对象
 		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
-		//创建回话工厂对象
+		//创建会话工厂对象
 		sessionFactory = config.buildSessionFactory(serviceRegistry);
-		//回话对象
+		//会话对象
 		session = sessionFactory.openSession();
 		//开启事务
 		transaction = session.beginTransaction();
@@ -31,15 +31,40 @@ public class UserTest {
 	@After
 	public void destory(){
 		transaction.commit(); //提交事务
-		session.close(); //关闭回话
-		sessionFactory.close(); //关闭回话工厂
+		session.close(); //关闭会话，若不释放可能导致数据库连接池溢出
+		sessionFactory.close(); //关闭会话工厂
 		
 	}
 	@Test
-	public void testSaveUser(){
+	public void testSaveUser(){     //増
 		//生成用户对象
-		User user = new User("黄复贵咯咯","123","1151650717@qq.com","sz",20,"13051189772","no");
+		User user = new User("黄复贵咯咯哈哈","123456","1151650717@qq.com","sz",20,"13051189772","no");
 		session.save(user); //保存对象进入数据库
 		
+	}
+	
+	@Test
+	public void testGetUser(){    //查
+		User user = (User)session.get(User.class,5);
+		System.out.println(user.getHeadimg());
+	}
+	
+	@Test
+	public void testLoadUser(){    //查
+		User user = (User)session.load(User.class,5);
+		System.out.println(user.getEmail());
+	}
+	
+	@Test
+	public void testUpdateUser(){     //改
+		User user = (User)session.get(User.class,5);
+		user.setPassword("nolala");
+		session.update(user);
+	}
+	
+	@Test
+	public void testDeleteUser(){     //删
+		User user = (User)session.get(User.class,17);
+		session.delete(user);
 	}
 }
