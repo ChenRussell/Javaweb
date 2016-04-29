795f7c001a24d2e1edb0e35f18429d9ef0ccca9b
HuangFuGui <1151650717@qq.com>
2016-04-29 15:05:18 +0800

    hibernate more to one map

--- Hibernate/hibernateDemo2/src/com/Demo2/test/more2one.java
+++ Hibernate/hibernateDemo2/src/com/Demo2/test/more2one.java
@@ -50,11 +50,22 @@ public class more2one {
 		Student stu1 = new Student("Russell.H","男");
 		Student stu2 = new Student("杨千嬅咯","女");
 		
-		stu1.setGrade(g);    /*将学生（多）加入班级（一）中*/
+		stu1.setGrade(g);    /*将学生（多）加入班级（一）中，单向*/
 		stu2.setGrade(g);
 		
 		session.save(g);
 		session.save(stu1);
 		session.save(stu2);
+	}
+	
+	@Test
+	public void getGradeByStudent(){
+		
+		Student stu = (Student)session.get(Student.class, 32);
+		System.out.println("学生姓名："+stu.getSname()+"   性别："+stu.getSex());
+		
+		Grade g = stu.getGrade();   //多对一关联关系
+		System.out.println("所在班级："+g.getGname());
+		
 	}
 }
