481545094efa326215a731640b0f6b9463a575b1
HuangFuGui <1151650717@qq.com>
2016-05-24 19:27:40 +0800

    maven build project,integrate framework,design and test DAO

---
+++ SpringMVC+Spring+MyBatis/seckill/src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -0,0 +1,26 @@
+package org.seckill.dao;
+
+
+import org.apache.ibatis.annotations.Param;
+import org.seckill.entity.SuccessKilled;
+
+/**
+ * Created by Administrator on 2016/5/23.
+ */
+public interface SuccessKilledDao {
+
+    /**
+     * 插入购买明细，联合主键可过滤重复
+     * @param seckillId
+     * @param userPhone
+     * @return
+     */
+    int insertSuccessKilled(@Param("seckillId") int seckillId,@Param("userPhone") String userPhone);
+
+    /**
+     * 根据id查询SuccessKilled并携带秒杀产品对象实体
+     * @param seckillId
+     * @return
+     */
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") int seckillId,@Param("userPhone") String userPhone);
+}
